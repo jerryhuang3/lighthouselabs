a0cf3a6905a565ca328a799ef525a61fb11a705f
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-08 17:01:35 +0000

    tested chalk

--- w1d5/npmPackageExercise/index.js
+++ w1d5/npmPackageExercise/index.js
@@ -1,4 +1,42 @@
 var chalk = require("chalk");
 
-var message = "Hello " + chalk.yellow("World");
+var message = "Hello " + chalk.bgMagenta("World");
 console.log(message);
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
+
+
+// Use RGB colors in terminal emulators that support it.
+log(chalk.keyword('orange')('Yay for orange colored text!'));
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
+
+// Easily define your own themes:
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+
+const name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
+//=> 'Hello Sindre'
