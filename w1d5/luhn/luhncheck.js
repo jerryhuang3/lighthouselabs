e3f6b469b0f36b36a3a299a2d1c1c0b879a86ae9
jerryhuang3 <43212650+jerryhuang3@users.noreply.github.com>
2019-03-09 18:12:57 -0500

    Update luhncheck.js

--- w1d5/luhn/luhncheck.js
+++ w1d5/luhn/luhncheck.js
@@ -1,1 +1,40 @@
+// Test account number 79927398713
 
+// var account = ;
+
+// Reverse the account number, split into an array, and
+// map each array element into a number
+function luhn(n) {
+    var array = n.split("").reverse().map(Number);
+        
+    var empty = [];
+    for (i = 0; i < array.length; i++) {
+        if ((i + 1) % 2 === 0) {
+            var second = array[i] * 2;
+            empty.push(second % 10 + Math.floor(second / 10));
+        } else {
+            empty.push(array[i]);
+        }
+
+    }
+
+    var sum = 0;
+    for (i = 0; i < empty.length; i++) {
+        sum += empty[i];
+    }
+    return sum;
+}
+
+function check(number){
+    var a = number;
+    var answer = "";
+    if (luhn(a) % 10 === 0) {
+        return answer = "This number is valid.";
+    } else {
+        return answer = "This number is not valid.";
+    }
+}
+
+module.exports = {
+    check: check
+}
